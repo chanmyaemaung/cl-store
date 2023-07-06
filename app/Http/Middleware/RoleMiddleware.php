4382d7fd03a6fe02737775097f0e76862e45be42
Conor <conor@visibleone.com>
2023-07-06 11:55:20 +0800

    Middleware: Fix route redirect middlaware for temporary :bug:

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -16,7 +16,7 @@ class RoleMiddleware
     public function handle(Request $request, Closure $next, $role): Response
     {
         if ($request->user()->role !== $role) {
-            return redirect()->route('dashboard');
+            return redirect()->route('user.dashboard');
         }
 
         return $next($request);
